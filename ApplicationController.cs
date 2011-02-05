90273f74083a568093cb82a228493799930383fe
Vahe <margoussian+github@gmail.com>
2011-02-05 11:27:55 -0800

    first commit

---
+++ ApplicationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simplisticky {
+    class ApplicationController {
+
+        private List<StickyNote> notelist;
+        private XmlController xml;
+
+        public ApplicationController() {
+            
+            notelist = new List<StickyNote>();
+            xml = new XmlController(this);
+            Initialize init = new Initialize(this);
+            init.begin();
+        }
+
+        // Accessors
+
+        public List<StickyNote> Notelist {
+            get {
+                return notelist;
+            }
+            set {
+                notelist = value;
+            }
+        }
+        
+        public XmlController Xml {
+            get {
+                return xml;
+            }
+            set {
+                xml = value;
+            }
+        }
+
+    }
+}
